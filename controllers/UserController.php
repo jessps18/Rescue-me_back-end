34ecbca9323b456e6b5e8706e4798efb561ab919
jessica pereira <annaeelza@gmail.com>
2026-05-15 22:01:12 -0300

    back finalizado

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,313 +3,314 @@
 require_once("banco.php");
 require_once("messages.php");
 
-//controller objeto
-function getBuscarObjeto($conn, $id){
+class UserController
+{
+    //controller objeto
+    function getBuscarObjeto($conn, $id, $usuarioLogado)
+    {
+        $idObjeto = $id;
 
-    $idObjeto=$id;
+        $objetoJSON = [];
 
-    $objetoJSON=[];
-
-    if(
-        $idObjeto == "" || $idObjeto==null || !is_numeric($idObjeto)){
-
-        return ERROR_INVALID;
-
-    }else{
-
-        $dadosObjeto= Objeto::get($conn, $id);
-
-        if($dadosObjeto){
-
-            $objetoJSON["objeto"]= $dadosObjeto;
-
-            $objetoJSON["status"]= 200;
-
-            return $objetoJSON;
-
-        }else{
-
-            return ERROR_NOT_FOUND;
-
+        if ($idObjeto == "" || $idObjeto == null || !is_numeric($idObjeto)) {
+            return ERROR_INVALID;
         }
 
-    }
-}
+        $dadosObjeto = Objeto::get($conn, $idObjeto);
 
-function getObjeto($conn){
-
-    $dadosObjeto = Objeto::all($conn);
-
-    if(!$dadosObjeto){
-
-        return ERROR_INTERNAL_SERVER_DB;
-    }
-
-    if(count($dadosObjeto)==0){
-
-        return ERROR_NOT_FOUND;
-    }
-
-    return [
-        "objetos"=>$dadosObjeto,
-        "status"=>200
-    ];
-}
-
-function updateObjeto($conn, $data){
-
-    if (empty($data['id_objeto'])) {
-        return ERROR_INVALID;
-    }
-
-    $dataObjeto = Objeto::recuperar($conn, $data);
-    if ($dataObjeto) {
-        return SUCCESS_UPDATED_ITEM;
-    }
-
-    return ERROR_INTERNAL_SERVER_DB;
-
-}
-
-function createObjeto($conn, $data){
-    if (empty($data['nome_objeto']) || empty($data['descricao']) || empty($data['local_encontrado']) || empty($data['onde_deixou']) || empty($data['encontrado_por'])) {
-        return ERROR_INVALID;
-    }
-
-    $dataObjeto = Objeto::create($conn, $data);
-    if ($dataObjeto) {
-        return SUCCESS_CREATED_ITEM;
-    }
-
-    return ERROR_INTERNAL_SERVER_DB;
-}
-
-//controller pessoas
-function getPessoas($conn){
-
-    $dadosPessoas = Pessoa::all($conn);
-
-    if(!$dadosPessoas){
-
-        return ERROR_INTERNAL_SERVER_DB;
-    }
-
-    if(count($dadosPessoas)==0){
-
-        return ERROR_NOT_FOUND;
-    }
-
-    return SUCCESS_CREATED_ITEM;
-}
-
-//controller Funcionario
-function getBuscarFuncionario($conn, $id){
-
-    $idFuncionario=$id;
-
-    $objetoJSON=[];
-
-    if(
-        $idFuncionario == "" || $idFuncionario==null || !is_numeric($idFuncionario)){
-
-        return ERROR_INVALID;
-
-    }else{
-
-        $dadosFuncionario= Funcionario::get($conn, $id);
-
-        if($dadosFuncionario){
-            return $objetoJSON;
-
-        }else{
-
+        if (!$dadosObjeto) {
             return ERROR_NOT_FOUND;
-
         }
 
-    }
-}
+        if (
+            $dadosObjeto['encontrado_por'] != $usuarioLogado
+        ) {
+            return ERROR_NOT_FOUND;
+        }
 
-//controller usuarios(alunos)
-function createUsuarios($conn, $data){
-    if (
-        empty($data['nome']) ||
-        empty($data['email']) ||
-        empty($data['senha']) ||
-        empty($data['numero_contato']) ||
-        empty($data['registro_academico']) ||
-        empty($data['curso_id'])
-    ) {
-
-        return ERROR_INVALID;
+        return $objetoJSON;
     }
 
-    $idPessoa = Pessoa::create(
-        $conn,
-        [
+    function getObjeto($conn)
+    {
 
-            "nome" => $data['nome'],
-            "email" => $data['email'],
-            "senha" => $data['senha'],
-            "numero_contato" =>$data['numero_contato'],
-            "tipo" => "USUARIO"
+        $dadosObjeto = Objeto::all($conn);
 
-        ]
-    );
+        if (!$dadosObjeto) {
 
-    if (!$idPessoa) {
+            return ERROR_INTERNAL_SERVER_DB;
+        }
+
+        if (count($dadosObjeto) == 0) {
+
+            return ERROR_NOT_FOUND;
+        }
+
+        return [
+            "objetos" => $dadosObjeto,
+            "status" => 200
+        ];
+    }
+
+    function updateObjeto($conn, $data)
+    {
+
+        if (empty($data['id_objeto'])) {
+            return ERROR_INVALID;
+        }
+
+        $dataObjeto = Objeto::recuperar($conn, $data);
+        if ($dataObjeto) {
+            return SUCCESS_UPDATED_ITEM;
+        }
 
         return ERROR_INTERNAL_SERVER_DB;
     }
 
-    $usuario = Usuario::create(
-        $conn,
-        [
-            "pessoa_id" => $idPessoa,
-            "registro_academico" => $data['registro_academico'],
-            "curso_id" => $data['curso_id']
+    function createObjeto($conn, $data)
+    {
+        if (empty($data['nome_objeto']) || empty($data['descricao']) || empty($data['local_encontrado']) || empty($data['onde_deixou']) || empty($data['encontrado_por'])) {
+            return ERROR_INVALID;
+        }
 
-        ]
-    );
+        $dataObjeto = Objeto::create($conn, $data);
+        if ($dataObjeto) {
+            return SUCCESS_CREATED_ITEM;
+        }
 
-    if ($usuario) {
+        return ERROR_INTERNAL_SERVER_DB;
+    }
+
+    //controller pessoas
+    function getPessoas($conn)
+    {
+
+        $dadosPessoas = Pessoa::all($conn);
+
+        if (!$dadosPessoas) {
+
+            return ERROR_INTERNAL_SERVER_DB;
+        }
+
+        if (count($dadosPessoas) == 0) {
+
+            return ERROR_NOT_FOUND;
+        }
 
         return SUCCESS_CREATED_ITEM;
     }
 
-    return ERROR_INTERNAL_SERVER_DB;
-}
+    //controller Funcionario
+    function getBuscarFuncionario($conn, $id, $usuarioLogado)
+    {
+        $idFuncionario = $id;
 
-function getBuscarUsuario($conn, $id){
+        $objetoJSON = [];
 
-    $idUsuario=$id;
+        if ($idFuncionario == "" || $idFuncionario == null || !is_numeric($idFuncionario)) {
 
-    $objetoJSON=[];
-
-    if(
-        $idUsuario == "" || $idUsuario==null || !is_numeric($idUsuario)){
-
-        return ERROR_INVALID;
-
-    }else{
-
-        $dadosUsuario= Usuario::get($conn, $id);
-
-        if($dadosUsuario){
-            return $objetoJSON;
-
-        }else{
-
-            return ERROR_NOT_FOUND;
-
+            return ERROR_INVALID;
         }
 
+        $dadosFuncionario = Funcionario::get($conn, $idFuncionario);
+
+        if (!$dadosFuncionario) {
+            return ERROR_NOT_FOUND;
+        }
+
+        if (
+            $dadosFuncionario['pessoa_id'] != $usuarioLogado
+        ) {
+            return ERROR_NOT_FOUND;
+        }
+
+        return $objetoJSON;
     }
-}
+    //controller usuarios(alunos)
+    function createUsuarios($conn, $data)
+    {
+        if (
+            empty($data['nome']) ||
+            empty($data['email']) ||
+            empty($data['senha']) ||
+            empty($data['numero_contato']) ||
+            empty($data['registro_academico']) ||
+            empty($data['curso_id'])
+        ) {
 
-//cursos
-function getCursos($conn){
+            return ERROR_INVALID;
+        }
 
-    $dados = Materias::all($conn);
+        $idPessoa = Pessoa::create(
+            $conn,
+            [
 
-    if(!$dados){
+                "nome" => $data['nome'],
+                "email" => $data['email'],
+                "senha" => $data['senha'],
+                "numero_contato" => $data['numero_contato'],
+                "tipo" => "USUARIO"
+
+            ]
+        );
+
+        if (!$idPessoa) {
+
+            return ERROR_INTERNAL_SERVER_DB;
+        }
+
+        $usuario = Usuario::create(
+            $conn,
+            [
+                "pessoa_id" => $idPessoa,
+                "registro_academico" => $data['registro_academico'],
+                "curso_id" => $data['curso_id']
+
+            ]
+        );
+
+        if ($usuario) {
+
+            return SUCCESS_CREATED_ITEM;
+        }
 
         return ERROR_INTERNAL_SERVER_DB;
     }
 
-    if(count($dados)==0){
+    function getBuscarUsuario($conn, $id, $usuarioLogado)
+    {
+        $idUsuario = $id;
 
-        return ERROR_NOT_FOUND;
-    }
+        $objetoJSON = [];
 
-    return [
-        "objetos"=>$dados,
-        "status"=>200
-    ];
-}
-
-//denuncias
-function getBuscarDenuncia($conn, $id){
-
-    $idDenuncia=$id;
-
-    $objetoJSON=[];
-
-    if(
-        $idDenuncia == "" || $idDenuncia==null || !is_numeric($idDenuncia)){
-
-        return ERROR_INVALID;
-
-    }else{
-
-        $dadosDenuncia= Denuncias::get($conn, $id);
-
-        if($dadosDenuncia){
-
-            return $objetoJSON;
-
-        }else{
-
-            return ERROR_NOT_FOUND;
-
+        if ($idUsuario == "" || $idUsuario == null || !is_numeric($idUsuario)) {
+            return ERROR_INVALID;
         }
 
+        $dadosUsuario = Usuario::get($conn, $idUsuario);
+
+        if (!$dadosUsuario) {
+            return ERROR_NOT_FOUND;
+        }
+
+        if (
+            $dadosUsuario['pessoa_id'] != $usuarioLogado
+        ) {
+
+            return ERROR_NOT_FOUND;
+        }
+
+        return $objetoJSON;
     }
-}
 
-function getDenuncia($conn){
+    //cursos
+    function getCursos($conn)
+    {
 
-    $dadosDenuncia = Denuncias::all($conn);
+        $dados = Materias::all($conn);
 
-    if(!$dadosDenuncia){
+        if (!$dados) {
 
+            return ERROR_INTERNAL_SERVER_DB;
+        }
+
+        if (count($dados) == 0) {
+
+            return ERROR_NOT_FOUND;
+        }
+
+        return [
+            "objetos" => $dados,
+            "status" => 200
+        ];
+    }
+
+    //denuncias
+    function getBuscarDenuncia($conn, $id, $usuarioLogado)
+    {
+        $idDenuncia = $id;
+        $objetoJSON = [];
+
+        if (
+            $idDenuncia == "" || $idDenuncia == null || !is_numeric($idDenuncia)
+        ) {
+            return ERROR_INVALID;
+        }
+
+        $dadosDenuncia =
+            Denuncias::get($conn, $idDenuncia);
+
+        if (!$dadosDenuncia) {
+
+            return ERROR_NOT_FOUND;
+        }
+
+        if (
+            $dadosDenuncia['denunciante'] != $usuarioLogado
+        ) {
+            return ERROR_NOT_FOUND;
+        }
+
+        return $objetoJSON;
+    }
+
+    function getDenuncia($conn)
+    {
+
+        $dadosDenuncia = Denuncias::all($conn);
+
+        if (!$dadosDenuncia) {
+
+            return ERROR_INTERNAL_SERVER_DB;
+        }
+
+        if (count($dadosDenuncia) == 0) {
+
+            return ERROR_NOT_FOUND;
+        }
+
+        return [
+            "objetos" => $dadosDenuncia,
+            "status" => 200
+        ];
+    }
+
+    function updateDenuncia($conn, $data)
+    {
+        if (
+            empty($data['id_denuncia']) || empty($data['status_denuncia']) || empty($data['analisado_por'])
+        ) {
+            return ERROR_INVALID;
+        }
+
+        $dadosDenuncia =
+            Denuncias::resolver(
+                $conn,
+                [
+                    "id_denuncia" => $data['id_denuncia'],
+                    "status_denuncia" => $data['status_denuncia'],
+                    "justificativa" => $data['justificativa'] ?? null,
+                    "analisado_por" => $data['analisado_por']
+                ]
+            );
+
+        if ($dadosDenuncia) {
+            return SUCCESS_UPDATED_ITEM;
+        }
         return ERROR_INTERNAL_SERVER_DB;
     }
 
-    if(count($dadosDenuncia)==0){
+    function createDenuncia($conn, $data)
+    {
+        if (empty($data['titulo']) || empty($data['descricao']) || empty($data['objeto_id']) || empty($data['denunciante']) || empty($data['denunciado'])) {
+            return ERROR_INVALID;
+        }
 
-        return ERROR_NOT_FOUND;
+        $dataDenuncia = Denuncias::create($conn, $data);
+        if ($dataDenuncia) {
+            return SUCCESS_CREATED_ITEM;
+        }
+
+        return ERROR_INTERNAL_SERVER_DB;
     }
-
-    return [
-        "objetos"=>$dadosDenuncia,
-        "status"=>200
-    ];
-}
-
-function updateDenuncia($conn, $data){
-    if(
-        empty($data['id_denuncia']) || empty($data['status_denuncia']) || empty($data['analisado_por'])
-    ){
-        return ERROR_INVALID;
-    }
-
-    $dadosDenuncia =
-    Denuncias::resolver(
-        $conn,
-        [
-            "id_denuncia" => $data['id_denuncia'],
-            "status_denuncia" =>$data['status_denuncia'],
-            "justificativa" => $data['justificativa'] ?? null,
-            "analisado_por" => $data['analisado_por']
-        ]
-    );
-
-    if($dadosDenuncia){
-        return SUCCESS_UPDATED_ITEM;
-    }
-    return ERROR_INTERNAL_SERVER_DB;
-}
-
-function createDenuncia($conn, $data){
-    if (empty($data['titulo']) || empty($data['descricao']) || empty($data['objeto_id']) || empty($data['denunciante']) || empty($data['denunciado'])) {
-        return ERROR_INVALID;
-    }
-
-    $dataDenuncia = Denuncias::create($conn, $data);
-    if ($dataDenuncia) {
-        return SUCCESS_CREATED_ITEM;
-    }
-
-    return ERROR_INTERNAL_SERVER_DB;
 }
--- index.php
+++ index.php
@@ -2,21 +2,107 @@
 
 require 'db.php';
 require 'controllers/UserController.php';
+session_start();
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
+$partes = explode('/', $uri);
+
+$id = $partes[2] ?? null;
 
 header('Content-Type: application/json');
 
-if ($uri === '/users' && $method === 'GET') {
-    (new UserController($conn))->index();
+//denuncias
+if ($uri === '/Denuncias' && $method === 'GET') {
+    (new UserController($conn))->getDenuncia($conn);
 }
 
-elseif ($uri === '/users' && $method === 'POST') {
-    (new UserController($conn))->store();
+/*elseif ($uri === '/Denuncia' && $method === 'GET') {
+    (new UserController($conn))->getBuscarDenuncia($conn, $id, $usuarioLogado);
+}
+*/
+
+elseif ($uri === '/DenunciasP' && $method === 'POST') {
+    $data = json_decode( file_get_contents("php://input"), true);
+
+    $response = (new UserController($conn))->createDenuncia($conn, $data);
+    echo json_encode($response);
+}
+
+elseif ($uri === '/DenunciasA' && $method === 'PUT') {
+    $data = json_decode( file_get_contents("php://input"), true);
+
+    $response = (new UserController($conn))->updateDenuncia($conn, $data);
+    echo json_encode($response);
+}
+
+elseif ($uri === '/Cursos' && $method === 'GET') {
+    (new UserController($conn))->getCursos($conn);
+}
+
+elseif ($uri === '/Usuarios' && $method === 'POST') {
+    $data = json_decode( file_get_contents("php://input"), true);
+
+    $response = (new UserController($conn))->createUsuarios($conn, $data);
+    echo json_encode($response);
+}
+
+elseif ($uri === '/Todos' && $method === 'GET') {
+    (new UserController($conn))->getPessoas($conn);
+}
+
+elseif ($uri === '/Objeto' && $method === 'POST') {
+    $data = json_decode( file_get_contents("php://input"), true);
+
+    $response = (new UserController($conn))->createObjeto($conn, $data);
+    echo json_encode($response);
+}
+
+elseif ($uri === '/ObjetoA' && $method === 'PUT') {
+    $data = json_decode( file_get_contents("php://input"), true);
+
+    $response = (new UserController($conn))->updateObjeto($conn, $data);
+    echo json_encode($response);
+}
+
+elseif ($uri === '/Objetos' && $method === 'GET') {
+    (new UserController($conn))->getObjeto($conn);
+}
+
+elseif ($partes[1] === 'Denuncia' && $method === 'GET') {
+    $usuarioLogado =
+    $_SESSION['usuario_id'] ?? null;
+    $response = (new UserController($conn)) ->getBuscarDenuncia($conn, $id, $usuarioLogado);
+
+    echo json_encode($response);
+}
+
+elseif ($partes[1] === 'Usuario' && $method === 'GET') {
+    $usuarioLogado =
+    $_SESSION['usuario_id'] ?? null;
+    $response = (new UserController($conn)) ->getBuscarUsuario($conn, $id, $usuarioLogado);
+
+    echo json_encode($response);
+}
+
+elseif ($partes[1] === 'Funcionario' && $method === 'GET') {
+    $usuarioLogado =
+    $_SESSION['usuario_id'] ?? null;
+    $response = (new UserController($conn)) ->getBuscarFuncionario($conn, $id, $usuarioLogado);
+
+    echo json_encode($response);
+}
+
+elseif ($partes[1] === 'Objeto' && $method === 'GET') {
+    $usuarioLogado =
+    $_SESSION['usuario_id'] ?? null;
+    $response = (new UserController($conn)) ->getBuscarObjeto($conn, $id, $usuarioLogado);
+
+    echo json_encode($response);
 }
 
 else {
     http_response_code(404);
     echo json_encode(["error" => "Rota não encontrada"]);
 }
+
